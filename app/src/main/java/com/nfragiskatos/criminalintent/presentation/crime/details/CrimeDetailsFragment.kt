0b3ec7950c8454c31697abc71800a62cb9ac33de
Nicholas Fragiskatos <nicholas.fragiskatos@pm.me>
2023-06-28 21:44:41 -0700

    - created fragment for fragment_crime_details.xml layout - added fragment container view to activity_main.xml layout to host the details fragment

---
+++ app/src/main/java/com/nfragiskatos/criminalintent/presentation/crime/details/CrimeDetailsFragment.kt
@@ -0,0 +1,56 @@
+package com.nfragiskatos.criminalintent.presentation.crime.details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
+import com.nfragiskatos.criminalintent.databinding.FragmentCrimeDetailsBinding
+import com.nfragiskatos.criminalintent.domain.Crime
+import java.util.Date
+import java.util.UUID
+
+class CrimeDetailsFragment : Fragment() {
+
+    private lateinit var binding: FragmentCrimeDetailsBinding
+    private lateinit var crime: Crime
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        crime = Crime(
+            UUID.randomUUID(),
+            title = "",
+            date = Date(),
+            isSolved = false
+        )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCrimeDetailsBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            crimeTitle.doOnTextChanged {text, _, _, _ ->
+                crime = crime.copy(title = text.toString())
+            }
+
+            crimeDate.apply {
+                text = crime.date.toString()
+                isEnabled = false
+            }
+
+            crimeSolved.setOnCheckedChangeListener { _, isChecked ->
+                crime = crime.copy(isSolved = isChecked)
+            }
+        }
+    }
+}
