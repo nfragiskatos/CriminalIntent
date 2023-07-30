31a98274df7b246fbd10653f6419879e368e51f0
Nicholas Fragiskatos <nicholas.fragiskatos@pm.me>
2023-07-30 16:18:03 -0700

    - initial detail data gets loaded when navigating to the detail screen - user updates get persisted back to the viewmodel

--- app/src/main/java/com/nfragiskatos/criminalintent/presentation/crime/details/CrimeDetailsFragment.kt
+++ app/src/main/java/com/nfragiskatos/criminalintent/presentation/crime/details/CrimeDetailsFragment.kt
@@ -7,9 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.navArgs
 import com.nfragiskatos.criminalintent.databinding.FragmentCrimeDetailsBinding
 import com.nfragiskatos.criminalintent.domain.Crime
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
 
@@ -18,23 +24,15 @@ class CrimeDetailsFragment : Fragment() {
 
     private var _binding : FragmentCrimeDetailsBinding? = null
     private val args: CrimeDetailsFragmentArgs by navArgs()
+
+    private val viewModel: CrimeDetailsViewModel by viewModels {
+        CrimeDetailsViewModelFactory(args.crimeId)
+    }
+
     private val binding
         get() = checkNotNull(_binding) {
             "Cannot access binding because it is null. Is the view visible?"
         }
-    private lateinit var crime: Crime
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        crime = Crime(
-            UUID.randomUUID(),
-            title = "",
-            date = Date(),
-            isSolved = false
-        )
-
-        Log.d(TAG, "The crime ID is: ${args.crimeId}")
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -50,16 +48,27 @@ class CrimeDetailsFragment : Fragment() {
 
         binding.apply {
             crimeTitle.doOnTextChanged {text, _, _, _ ->
-                crime = crime.copy(title = text.toString())
+                viewModel.updateCrime {oldCrime ->
+                    oldCrime.copy(title = text.toString())
+                }
             }
 
             crimeDate.apply {
-                text = crime.date.toString()
                 isEnabled = false
             }
 
             crimeSolved.setOnCheckedChangeListener { _, isChecked ->
-                crime = crime.copy(isSolved = isChecked)
+                viewModel.updateCrime { oldCrime ->
+                    oldCrime.copy(isSolved = isChecked)
+                }
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.crime.collect {crime ->
+                    crime?.let {updateUi(crime)  }
+                }
             }
         }
     }
@@ -67,5 +76,15 @@ class CrimeDetailsFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun updateUi(crime: Crime) {
+        binding.apply {
+            if (crimeTitle.text.toString() != crime.title) {
+                crimeTitle.setText(crime.title)
+            }
+            crimeDate.text = crime.date.toString()
+            crimeSolved.isChecked = crime.isSolved
+        }
     }
 }
---
+++ app/src/main/java/com/nfragiskatos/criminalintent/presentation/crime/details/CrimeDetailsViewModel.kt
@@ -0,0 +1,39 @@
+package com.nfragiskatos.criminalintent.presentation.crime.details
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.nfragiskatos.criminalintent.domain.Crime
+import com.nfragiskatos.criminalintent.domain.CrimeRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import java.util.UUID
+
+class CrimeDetailsViewModel(crimeId: UUID) : ViewModel() {
+    private val crimeRepository = CrimeRepository.get()
+
+    private val _crime : MutableStateFlow<Crime?> = MutableStateFlow(null)
+    val crime: StateFlow<Crime?> = _crime.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            _crime.value = crimeRepository.getCrime(crimeId)
+        }
+    }
+
+    fun updateCrime(onUpdate: (Crime) -> Crime) {
+        _crime.update { oldCrime ->
+            oldCrime?.let{onUpdate(it)}
+        }
+    }
+}
+
+class CrimeDetailsViewModelFactory(private val crimeId: UUID) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return CrimeDetailsViewModel(crimeId) as T
+    }
+
+}
